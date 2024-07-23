33037a324a060cd4d9c00cde7ed5b9141c45354b
Nguyễn Minh Quang <95731193+nmquang1416@users.noreply.github.com>
2024-07-23 20:22:06 +0700

    update nuget

--- AssignmentCSharp02/obj/Debug/net8.0/AssignmentCSharp02.AssemblyInfo.cs
+++ AssignmentCSharp02/obj/Debug/net8.0/AssignmentCSharp02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssignmentCSharp02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3102f8d1143423e52d6f0801a77026695964da74")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssignmentCSharp02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssignmentCSharp02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
